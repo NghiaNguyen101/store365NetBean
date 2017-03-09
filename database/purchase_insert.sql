b0c7e669d1e9467c14f290f8abdf35b3b6d8f41e
Andrew Rose <paro42796@gmail.com>
2017-03-09 13:43:51 -0800

    Added purchase and transaction data for database

---
+++ database/purchase_insert.sql
@@ -0,0 +1,43 @@
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(1, 1, 10, 10);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(2, 4, 1, 30);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(2, 8, 1, 20);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(3, 2, 1, 7);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(3, 3, 1, 3);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(4, 5, 2, 15);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(5, 5, 1, 15);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(5, 4, 1, 30);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(5, 8, 1, 20);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(5, 9, 1, 5);
+
+INSERT INTO purchaseTable (transaction_id, item_id, amount, price)
+VALUES
+(5, 1, 1, 10);
---
+++ database/transaction_insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO transactionTable (customer_id, credit_card, amount, purchase_date)
+VALUES
+(1, '1234567891011121', 100, '2017-01-30');
+
+INSERT INTO transactionTable (customer_id, credit_card, amount, purchase_date)
+VALUES
+(2, '5555555555555555', 50, '2016-12-30');
+
+INSERT INTO transactionTable (customer_id, credit_card, amount, purchase_date)
+VALUES
+(3, '1111222233334444', 10, '2017-03-07');
+
+INSERT INTO transactionTable (customer_id, credit_card, amount, purchase_date)
+VALUES
+(4, '5555666677778888', 30, '2016-11-11');
+
+INSERT INTO transactionTable (customer_id, credit_card, amount, purchase_date)
+VALUES
+(5, '9999888877776666', 80, '2017-02-20');
